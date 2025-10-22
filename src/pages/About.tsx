30db81597a90d2cc6511598a03b860d9bf717e84
Geoff <geoffrey.pariseau311@gmail.com>
2025-10-22 11:15:15 -0700

    Updated About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -84,7 +84,7 @@ export default function About() {
               className="rounded-lg shadow-lg hover:scale-105 transition-transform duration-300 w-full h-64 object-cover"
             />
             <img
-              src="https://images.pexels.com/photos/1626481/pexels-photo-1626481.jpeg?auto=compress&cs=tinysrgb&w=600"
+              src="/309 Cover Art.png"
               alt="Live Performance"
               className="rounded-lg shadow-lg hover:scale-105 transition-transform duration-300 w-full h-64 object-cover"
             />
